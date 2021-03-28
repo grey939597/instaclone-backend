65b6955344f19b4419e0a17c74fbb5937fa540d5
goodgoodcoder <jongyunj@icloud.com>
2021-03-28 15:46:51 +0900

    #3.3 POC API

--- server.js
+++ server.js
@@ -2,15 +2,35 @@ import { ApolloServer, gql } from "apollo-server";
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "works",
+    movies: () => [],
+    movie: () => ({ title: "hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
