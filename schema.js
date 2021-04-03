09fea7eece11463d7d867fbc4abab70b88423404
goodgoodcoder <jongyunj@icloud.com>
2021-04-04 01:36:49 +0900

    #3.9-3.10 Architecture

---
+++ schema.js
@@ -0,0 +1,23 @@
+import {
+  loadFilesSync,
+  makeExecutableSchema,
+  mergeTypeDefs,
+  mergeResolvers,
+} from "graphql-tools";
+
+// ** : every folders, * : every filenames
+// loadFilesSync : export default로만 파일 불러오기 가능
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.js`
+);
+
+// The GraphQL schema
+const typeDefs = mergeTypeDefs(loadedTypes);
+
+// A map of functions which return data for the schema.
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
--- server.js
+++ server.js
@@ -1,56 +1,12 @@
-import { ApolloServer, gql } from "apollo-server";
-import { PrismaClient } from "@prisma/client";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const client = new PrismaClient();
-
-// The GraphQL schema
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-// A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) => {
-      return client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      });
-    },
-    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
-    updateMovie: (_, { id, year }) =>
-      client.movie.update({ where: { id }, data: { year } }),
-  },
-};
-
+// Create a server
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
+// Running a server
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at http://localhost:4000`);
 });
