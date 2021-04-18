5659a50a0e12973c14b5aafaa465d3bec939313c
goodgoodcoder <jongyunj@icloud.com>
2021-04-18 15:52:45 +0900

    #4.6 Divide and Conquer

--- schema.js
+++ schema.js
@@ -8,9 +8,7 @@ import {
 // ** : every folders, * : every filenames
 // loadFilesSync : export default로만 파일 불러오기 가능
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
-);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
 // The GraphQL schema
 const typeDefs = mergeTypeDefs(loadedTypes);
---
+++ users/login/login.resolvers.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+
+export default {
+  Mutation: {
+    login: async (_, { username, password }) => {
+      // (1) find user with args.username
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+
+      // (2) check password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+
+      // (3) issue a token and send it to the user
+      // Json Web Token (Server - Frontend 분리 되어 있을 때)
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
+    },
+  },
+};
