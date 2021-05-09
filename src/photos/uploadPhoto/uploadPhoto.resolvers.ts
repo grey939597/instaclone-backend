a112a6d30e33ec4a68ee35e41610547d338d6b56
goodgoodcoder <jongyunj@icloud.com>
2021-05-09 23:18:40 +0900

    #6.1-6.3 Upload Photo

---
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -0,0 +1,40 @@
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser, client }) => {
+        let hashtagObjs = [];
+        if (caption) {
+          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          hashtagObjs = hashtags.map((hashtag) => ({
+            where: {
+              hashtag,
+            },
+            create: {
+              hashtag,
+            },
+          }));
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObjs.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObjs,
+              },
+            }),
+          },
+        });
+        // save the photo with the parsed hashtags
+        // add the photo to the hashtags
+      }
+    ),
+  },
+};
