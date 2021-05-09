6e2a46a9a1c819976cc3b430b59ac9867dffe812
goodgoodcoder <jongyunj@icloud.com>
2021-05-09 23:36:45 +0900

    #6.5 seePhoto

---
+++ src/photos/photos.resolvers.ts
@@ -0,0 +1,20 @@
+export default {
+  Photo: {
+    user: ({ userId }, _, { client }) =>
+      client.user.findUnique({
+        where: {
+          id: userId,
+        },
+      }),
+    hashtags: ({ id }, _, { client }) =>
+      client.hashtag.findMany({
+        where: {
+          photos: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+  },
+};
---
+++ src/photos/seePhoto/seePhoto.resolvers.ts
@@ -0,0 +1,8 @@
+export default {
+  Query: {
+    seePhoto: (_, { id }, { client }) =>
+      client.photo.findUnique({
+        where: { id },
+      }),
+  },
+};
