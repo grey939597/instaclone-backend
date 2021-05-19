ff759f6bd3fb782a29fb897cc6bac3d5c26130cb
goodgoodcoder <jongyunj@icloud.com>
2021-05-19 22:48:41 +0900

    #6.18 protectedResolver Refactor

--- src/users/users.utils.ts
+++ src/users/users.utils.ts
@@ -25,22 +25,21 @@ export const getUser = async (token) => {
 
 // 함수를 return 하는 함수
 // 함수 (root, args, context, info) => { ... } 를 return
-export const protectedResolver = (ourResolver: Resolver) => (
-  root,
-  args,
-  context,
-  info
-) => {
-  // loggedInUser가 없으면 { ok: false, error: "..." }을 return
-  if (!context.loggedInUser) {
-    return {
-      ok: false,
-      error: "Please log in to perform this action.",
-    };
-  }
-  // loggedInUser가 있으면 ourResolver (실행할 resolver)를 return
-  return ourResolver(root, args, context, info);
-};
+export const protectedResolver =
+  (ourResolver: Resolver) => (root, args, context, info) => {
+    if (!context.loggedInUser) {
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
+    }
+    return ourResolver(root, args, context, info);
+  };
 
 // export default function protectedResolver(ourResolver) {
 //   return function (root, args, context, info) {
